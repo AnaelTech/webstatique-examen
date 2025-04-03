164ca90fc0212f579099ca36764807414c490fe1
Anael Payet <anaelpayetpro@gmail.com>
2025-04-03 15:51:28 +0200

    ✨ feat:  Update

--- src/app/tarif/tarif.component.ts
+++ src/app/tarif/tarif.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-tarif',
-  imports: [],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './tarif.component.html',
   styleUrl: './tarif.component.css'
 })
