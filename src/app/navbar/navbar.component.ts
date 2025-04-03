4a563f979134d73ac6642079c3c7b924d61ebf09
Anael Payet <anaelpayetpro@gmail.com>
2025-04-03 10:06:28 +0200

    ✨ feat:  Dropdown fonctionnel en mobile

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+isOpen: boolean = false;
+
+toggleMenu() {
+  this.isOpen = !this.isOpen;
+}
+
 
 }
